b112db2f9925e91b7e0bcce59e51f9034544b974
midvaz <mi28df@gmail.com>
2021-04-12 22:52:08 +0300

    2.1

--- neironka.py
+++ neironka.py
@@ -3,19 +3,20 @@ with open('new_book.txt','r', encoding='utf-8') as file:
         r = file.readlines()
         count = -1
         for i in r:
-            count +=1
-            print(count)
-            i = i.lstrip()
-            i = i.lower()
-            i = i.replace("\n",'')
-            i = i.replace("."," .PERIOD \n")
-            i = i.replace(","," ,COMMA ")
-            i = i.replace(":"," :COLON")
-            i = i.replace(";"," ;SEMICOLON")
-            i = i.replace("?"," ?QUESTION \n")
-            i = i.replace("!"," !EXCLAMATION \n")
-            i = i.replace("..."," ...ELIPSIS \n")
+            if i != '':
+                count +=1
+                print(count)
+                i = i.lstrip()
+                i = i.lower()
+                i = i.replace("\n",'')
+                i = i.replace("."," .PERIOD \n")
+                i = i.replace(","," ,COMMA ")
+                i = i.replace(":"," :COLON")
+                i = i.replace(";"," ;SEMICOLON")
+                i = i.replace("?"," ?QUESTION \n")
+                i = i.replace("!"," !EXCLAMATION \n")
+                i = i.replace("..."," ...ELIPSIS \n")
 
-            new_file.write(i)
+                new_file.write(i)
         # except Exception as e:
         #     print('erroe is: ', e)
